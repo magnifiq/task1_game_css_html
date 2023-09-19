dddfba4b39383d949c26ca55bfd1633150cc9912
magnifiq <git.projectsss@gmail.com>
2023-09-19 13:33:19 +0200

    add

--- sass_practice/gulpfile.js
+++ sass_practice/gulpfile.js
@@ -1,14 +1,16 @@
 const {src, dest, watch, series}=require('gulp') //we import here methods that we will use
 const sass=require('gulp-sass')(require('sass')) //we set here sass
+const purgecss=require('gulp-purgecss');
 
 function buildStyles(){
     return src('shinobi/**/*.scss')
     .pipe(sass())
+    .pipe(purgecss({content:['*.html']}))
     .pipe(dest('css'))
 }
 
 function watchTask(){
-    watch(['shinobi/**/*.scss'], buildStyles)
+    watch(['shinobi/**/*.scss', '*.html'], buildStyles)
 }
 
 exports.default=series(buildStyles,watchTask);
